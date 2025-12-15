7d355f2543f793cbac31ece3bfdc655e7541d471
UzuLaura <laura.uzusiene@gmail.com>
2025-12-15 08:44:51 +0200

    Improve read-more functionality for non-overflowing text

--- src/components/Films/List/Description/Description.tsx
+++ src/components/Films/List/Description/Description.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useRef, useEffect} from "react";
 import styles from "./Description.module.scss";
 import {Button} from "../../../Partials/Button/Button";
 
@@ -8,11 +8,31 @@ interface FilmListDescriptionProps {
 
 export const Description = ({text}: FilmListDescriptionProps) => {
     const [show, setShow] = useState(false);
+    const [showButton, setShowButton] = useState(false);
+    const textRef = useRef<HTMLParagraphElement>(null);
+
+    useEffect(() => {
+        const el = textRef.current;
+        if (!el) return;
+
+        setShowButton(el.scrollHeight > el.clientHeight);
+
+        const observer = new ResizeObserver(() => {
+            setShowButton(el.scrollHeight > el.clientHeight);
+        });
+
+        observer.observe(el);
+
+        return () => observer.disconnect();
+    }, [text]);
 
     return (
         <div className={`${styles.container} ${show && styles.expanded}`}>
-            <p>{text}</p>
-            <Button action={() => setShow(!show)} title={show ? 'Read Less' : 'Read More'} srSkip={true} />
+            <p ref={textRef}>{text}</p>
+            {
+                showButton &&
+                <Button action={() => setShow(!show)} title={show ? 'Read Less' : 'Read More'} srSkip={true} />
+            }
         </div>
     )
 };
